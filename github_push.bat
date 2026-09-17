0e420b22841ca13bd95549c7401d94e979ab6bb7
suqer34 <bilgiislem@alnusyatirim.com>
2026-09-17 13:25:20 +0300

    Update github_push.bat script encoding

--- github_push.bat
+++ github_push.bat
@@ -1,15 +1,15 @@
 @echo off
 chcp 65001 > nul
 echo =======================================================
-echo PING UYARI - GITHUB REPO GÜNCELLEME BETİĞİ
+echo PING UYARI - GITHUB REPO GUNCELLEME BETIGI
 echo =======================================================
 echo.
-echo 1. Git değişiklikleri çekiliyor...
+echo 1. Git degisiklikleri cekiliyor...
 git pull origin main --rebase
 echo.
-echo 2. Güncellemeler GitHub'a yükleniyor...
+echo 2. Guncellemeler GitHub'a yukleniyor...
 git push -u origin main
 echo.
-echo İŞLEM TAMAMLANDI!
+echo ISLEM TAMAMLANDI!
 echo.
 pause
