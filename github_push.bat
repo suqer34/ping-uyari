f2d6c4c7ce5e0c58568227972c0ab29888d5628c
suqer34 <bilgiislem@alnusyatirim.com>
2026-09-17 13:24:31 +0300

    Update gitignore and push script

--- github_push.bat
+++ github_push.bat
@@ -1,9 +1,15 @@
 @echo off
 chcp 65001 > nul
 echo =======================================================
-echo PING UYARI - GITHUB REPO GÜNCELLEME YÜKLEME BETİĞİ
+echo PING UYARI - GITHUB REPO GÜNCELLEME BETİĞİ
 echo =======================================================
 echo.
+echo 1. Git değişiklikleri çekiliyor...
+git pull origin main --rebase
+echo.
+echo 2. Güncellemeler GitHub'a yükleniyor...
 git push -u origin main
+echo.
+echo İŞLEM TAMAMLANDI!
 echo.
 pause
